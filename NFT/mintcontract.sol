f6eb012402bf6599582bac32421fc543e548f43d
Rian Pollock <112873791+rpollock@users.noreply.github.com>
2024-01-23 10:01:46 +0000

    Update mintcontract.sol

--- NFT/mintcontract.sol
+++ NFT/mintcontract.sol
@@ -11,6 +11,7 @@ contract SimpleMintContract is ERC721, Ownable {
     bool public isMintEnabled;
     mapping(address => uint256) public mintedWallets;
 
+//@dev ('Name of NFT', 'NFT token symbol') vvv
     constructor() payable ERC721('Simple Mint', 'SIMPLEMINT') {
         maxSupply = 2;
     }
